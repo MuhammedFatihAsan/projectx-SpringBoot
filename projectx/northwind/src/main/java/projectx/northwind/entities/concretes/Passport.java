7d25468ec32431fdaf85236215d82c11571109f2
MuhammedFatihAsan <m.fatihasan@hotmail.com>
2025-07-11 09:10:35 +0300

    Update Passport.java

--- projectx/northwind/src/main/java/projectx/northwind/entities/concretes/Passport.java
+++ projectx/northwind/src/main/java/projectx/northwind/entities/concretes/Passport.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 @Entity
 @Table(name="passport")
